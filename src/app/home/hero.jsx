f946da8b8a3b9ae240ae2f236e058a177c7b33ff
Akpa Onyedikachukwu <akpa3220@gmail.com>
2024-04-23 00:06:10 +0100

    flow

--- src/app/home/hero.jsx
+++ src/app/home/hero.jsx
@@ -2,22 +2,55 @@
 import Image from "next/image";
 import Lottie from "lottie-react";
 import anim from "../../../public/ai.json";
+import { animate, motion } from "framer-motion";
+
+const banner = {
+  animate: {
+    transition: {
+      delayChildern: 0.4,
+      staggerChildren: 0.1,
+    },
+  },
+};
+
+const letterAnimation = {
+  initial: { opacity: 0, y: 100 },
+  animate: { opacity: 1, y: 0 },
+  transition: {
+    ease: [0.6, 0.01, -0.05, 0.95],
+    duration: 1,
+  },
+};
 
 export default function Hero() {
   return (
-    <main className='text-text  ref lg:flex w-full lg:min-h-[80vh]'>
+    <main className='text-text relative isolate ref lg:flex w-full lg:min-h-[80vh]'>
+      <marquee
+        className='absolute w-full h-20 z-10 font-medium text-3xl lg:text-6xl text-primary/40 top-[42%] lg:top-1/2 bottom-1/2 '
+        behavior=''
+        direction='left'
+      >
+        Empower Your Business with Quanta
+      </marquee>
       <section className='lg:flex justify-between relative mt-5'>
-        <div className='basis-1/5 -z-10 flex items-end'>
-          <p className='mt-10 lg:text-7xl text-4xl lg:text-left text-center font-bold text-primary'>
-            AI DRIVEN DATA ANALYTICS
-          </p>
-        </div>
+        <div className='basis-1/5 lg:block hidden' />
+        <motion.div
+          variants={banner}
+          className='basis-1/5 lg:absolute lg:mt-0 mt-16 h-full -z-10 flex items-end'
+        >
+          <AnimatedLetters title={"AI-DRIVEN-DATA-ANALYTICS"} />
+        </motion.div>
 
         <div className='basis-2/5 flex items-center lg:my-0 my-10 justify-center'>
           <Lottie className='lg:h-3/4 h-64' animationData={anim} />
         </div>
 
-        <div className='basis-1/5 lg:flex flex-col items-end'>
+        <motion.div
+          initial={{ opacity: 0, y: 80 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ ease: "easeInOut", duration: 1, delay: 0.4 }}
+          className='basis-1/5 lg:flex flex-col items-end'
+        >
           <p className=' lg:text-right lg:text-base text-sm leading-loose'>
             Where data meets destiny, Quanta pioneers the fusion of AI and
             analytics. Dive into the future of data-driven decision-making with
@@ -27,7 +60,7 @@ export default function Hero() {
             brighter tomorrow.
           </p>
 
-          <button className='mt-10 flex lg:py-4 py-3 px-6 lg:px-10 bg-primary rounded-full font-bold text-black'>
+          <button className='mt-10 lg:mt-20 flex lg:py-4 py-3 px-6 lg:px-10 bg-primary rounded-full font-bold text-black'>
             Get Started
             <svg
               xmlns='http://www.w3.org/2000/svg'
@@ -48,8 +81,27 @@ export default function Hero() {
               <path d='M13 6l6 6' />
             </svg>
           </button>
-        </div>
+        </motion.div>
       </section>
     </main>
   );
 }
+
+const AnimatedLetters = ({ title }) => (
+  <motion.span
+    className='lg:text-7xl overflow-hidden flex text-xl lg:text-left text-center font-bold text-primary'
+    variants={banner}
+    initial='initial'
+    animate='animate'
+  >
+    {[...title].map((letter, index) => (
+      <motion.span
+        className='overflow-y-hidden'
+        variants={letterAnimation}
+        key={index}
+      >
+        {letter}
+      </motion.span>
+    ))}
+  </motion.span>
+);
